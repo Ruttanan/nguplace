ae3cdc6d230698470db589ceb4dd25172df31f1b
Ruttanan <ruttanan.w@gmail.com>
2025-06-14 13:27:45 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+# Load configs for local hosting
+path = 'config.json'
+if os.path.isfile(path): # <-- file won't exist in production
+  with open(path) as f: 
+    config = json.loads(f.read())
+  for key, value in config.items():
+    os.environ[key] = value
+  os.environ['test'] = '1'
+
+# Validate keys
+keys = ('START', 'DB', 'GLOBAL', 'LOCAL', 'HOUR', 'DAY', 'WEEK', 'TOKEN')
+assert all(key in os.environ for key in keys), 'fail key'
+
+# Validate pass
+if os.getenv('START') != '123':
+  exit()
+
+from src.server import run
+
+if os.getenv('test'):
+  print('''
+         ______  _                   _ 
+   _ __ / /  _ \| | __ _  ___ ___ 2 | |
+  | '__/ /| |_) | |/ _` |/ __/ _ \  | |
+  | | / / |  __/| | (_| | (_|  __/  |_|
+  |_|/_/  |_|   |_|\__,_|\___\___|  (_)
+  ''')
+
+# Run the bot
+app = run()
